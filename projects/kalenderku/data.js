a8f7f965cbf1a06c33ee4e8337dde97eb78b0ae9
tuskwt <smksukawatis@gmail.com>
2026-01-03 13:03:10 +0700

    Add files via upload

---
+++ projects/kalenderku/data.js
@@ -0,0 +1,138 @@
+window.calendarData = {
+    "year": 2026,
+    "workDays": 5,
+    "events": {
+        "2026-01-01": {
+            "label": "Tahun Baru Masehi 2026",
+            "type": "red"
+        },
+        "2026-01-02": {
+            "label": "Libur Akhir Semester Gasal",
+            "type": "red"
+        },
+        "2026-01-03": {
+            "label": "Libur Akhir Semester Gasal",
+            "type": "red"
+        },
+        "2026-01-05": {
+            "label": "Hari Pertama Masuk Semester Genap",
+            "type": "green"
+        },
+        "2026-01-16": {
+            "label": "Isra Mikraj Nabi Muhammad SAW",
+            "type": "red"
+        },
+        "2026-02-17": {
+            "label": "Tahun Baru Imlek 2577 Kongzili",
+            "type": "red"
+        },
+        "2026-02-19": {
+            "label": "Perkiraan Awal Puasa",
+            "type": "blue"
+        },
+        "2026-03-19": {
+            "label": "Harl Suci Nyepi (Tahun Baru Saka 1948)",
+            "type": "red"
+        },
+        "2026-03-20": {
+            "label": "Cuti Bersama Idul Fitri 1447 Hijriah ",
+            "type": "red"
+        },
+        "2026-03-21": {
+            "label": "Idul Fitri 1447 Hijriah",
+            "type": "red"
+        },
+        "2026-03-23": {
+            "label": "Cuti Bersama Idul Fitri 1447 Hijriah ",
+            "type": "red"
+        },
+        "2026-03-24": {
+            "label": "Cuti Bersama Idul Fitri 1447 Hijriah ",
+            "type": "red"
+        },
+        "2026-05-01": {
+            "label": "Hari Buruh Internasional",
+            "type": "red"
+        },
+        "2026-05-27": {
+            "label": "Idul Adha 1447 Hijriah",
+            "type": "red"
+        },
+        "2026-06-01": {
+            "label": "Hari Lahir Pancasila",
+            "type": "red"
+        },
+        "2026-06-22": {
+            "label": "Perkiraan Mulai Libur Akhir Semester Genap",
+            "type": "blue"
+        },
+        "2026-03-18": {
+            "label": "Cuti Bersama Hari Suci Nyepi (Tahun Baru Saka 1948)",
+            "type": "red"
+        },
+        "2026-02-16": {
+            "label": "Cuti Bersama Tahun Baru Imlek 2577 Kongzili",
+            "type": "red"
+        },
+        "2026-03-22": {
+            "label": "Idul Fitri 1447 Hijriah",
+            "type": "red"
+        },
+        "2026-04-03": {
+            "label": "Wafat Yesus Kristus ",
+            "type": "red"
+        },
+        "2026-04-05": {
+            "label": "Kebangkitan Yesus Kristus (Paskah)",
+            "type": "red"
+        },
+        "2026-05-14": {
+            "label": "Hari Kenaikan Yesus Kristus",
+            "type": "red"
+        },
+        "2026-05-31": {
+            "label": "Hari Raya Waisak 2570 BE ",
+            "type": "red"
+        },
+        "2026-05-28": {
+            "label": "Cuti Bersama Idul Adha 1447 Hijriah",
+            "type": "red"
+        },
+        "2026-05-15": {
+            "label": "Cuti Bersama Kenaikan Yesus Kristus",
+            "type": "red"
+        },
+        "2026-06-16": {
+            "label": "1 Muharam Tahun Baru Islam 1448 Hijriah",
+            "type": "red"
+        },
+        "2026-08-17": {
+            "label": "Proklamasi Kemerdekaan",
+            "type": "green"
+        },
+        "2026-08-25": {
+            "label": "Maulid Nabi Muhammad S.A.W.",
+            "type": "red"
+        },
+        "2026-12-24": {
+            "label": "Cuti Bersama Kelahiran Yesus Kristus",
+            "type": "red"
+        },
+        "2026-12-25": {
+            "label": "Kelahiran Yesus Kristus",
+            "type": "red"
+        },
+        "2026-08-01": {
+            "label": "SMK Sukawati Sragen pertama kali berdiri pada 1 Agustus 1964 dengan nama “STM Persiapan Negeri Sragen\"",
+            "type": "red"
+        },
+        "2026-12-01": {
+            "label": "Nama kedua (dahulu sebelum SMK Sukawati Sragen): STM Persiapan I Sragen (1 Desember 1972)",
+            "type": "green"
+        },
+        "2026-10-01": {
+            "label": "Berganti nama \"STM/SMK Sukawati Sragen\" pada 1 Oktober 1976.",
+            "type": "green"
+        }
+    }
+};
